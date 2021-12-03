d549f818216140f1c5017346fb757f366ba57625
lobba111 <robinolsson@gmail.com>
2021-12-03 17:20:29 +0100

    Changed algoritm

--- PuzzleNr1/Program.cs
+++ PuzzleNr1/Program.cs
@@ -1,22 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] sampleData = new int[]{103,106,107,106,121,132,147,148,144,141};
+int[] sampleData = new int[]{199,200,208,210,200,207,240,269,260,263};
 List<int> increased = new List<int>();
 List<int> decreased = new List<int>();
 
-for (int i = 0; i < sampleData.Length-1; i++)
+for (int i = 1; i < sampleData.Length; i++)
 {
     
-    if (sampleData[i] < sampleData[i+1])
+    if (sampleData[i] > sampleData[i-1] )
     {
-        Console.WriteLine("INCREASED");
+        Console.WriteLine("{0} . INCREASED",sampleData[i]);
 
         increased.Add(i);
     }
-    else if (sampleData[i] >sampleData[i+1])
+    else if (sampleData[i] <sampleData[i+1])
     {
-        Console.WriteLine("decreased");
+        Console.WriteLine("{0} . decreased", sampleData[i]);
         decreased.Add(i);
     }
+    
 }
 Console.WriteLine(increased.Count);
 Console.WriteLine(decreased.Count);
