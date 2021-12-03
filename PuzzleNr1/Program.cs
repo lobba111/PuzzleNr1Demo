5dee4e737686ba1a45747656bc4a123a3e6f6126
lobba111 <robinolsson@gmail.com>
2021-12-03 18:17:25 +0100

    Trying to get a reader to work for my data input

--- PuzzleNr1/Program.cs
+++ PuzzleNr1/Program.cs
@@ -1,23 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] sampleData = new int[]{199,200,208,210,200,207,240,269,260,263};
+//HtmlAgilityPack.HtmlWeb website = new HtmlAgilityPack.HtmlWeb();
+//HtmlAgilityPack.HtmlDocument document = website.Load("https://adventofcode.com/2021/day/1/input");
+//var datalist =document.DocumentNode.SelectNodes("pre[@spill]/pre").ToArray();
+//foreach (var content in datalist)
+//{
+//    Console.WriteLine(content.InnerText);
+//}
+
+
+
+
+
+int[] sampleData = new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
 List<int> increased = new List<int>();
 List<int> decreased = new List<int>();
 
+
 for (int i = 1; i < sampleData.Length; i++)
 {
-    
-    if (sampleData[i] > sampleData[i-1] )
+
+    if (sampleData[i] > sampleData[i - 1])
     {
-        Console.WriteLine("{0} . INCREASED",sampleData[i]);
+        Console.WriteLine("{0} . INCREASED", sampleData[i]);
 
         increased.Add(i);
     }
-    else if (sampleData[i] <sampleData[i+1])
+    else if (sampleData[i] < sampleData[i + 1])
     {
         Console.WriteLine("{0} . decreased", sampleData[i]);
         decreased.Add(i);
     }
-    
+   
 }
 Console.WriteLine(increased.Count);
 Console.WriteLine(decreased.Count);
