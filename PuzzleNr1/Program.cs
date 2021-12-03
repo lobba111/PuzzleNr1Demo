05e6c073e5b1c5b762106e5cca044cf699ee4fd6
lobba111 <robinolsson@gmail.com>
2021-12-03 17:08:10 +0100

    Changed the sampleData

---
+++ PuzzleNr1/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] sampleData = new int[]{103,106,107,106,121,132,147,148,144,141};
+List<int> increased = new List<int>();
+List<int> decreased = new List<int>();
+
+for (int i = 0; i < sampleData.Length-1; i++)
+{
+    
+    if (sampleData[i] < sampleData[i+1])
+    {
+        Console.WriteLine("INCREASED");
+
+        increased.Add(i);
+    }
+    else if (sampleData[i] >sampleData[i+1])
+    {
+        Console.WriteLine("decreased");
+        decreased.Add(i);
+    }
+}
+Console.WriteLine(increased.Count);
+Console.WriteLine(decreased.Count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
